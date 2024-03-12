5515197402ea03cad0524aadbeffc3595101f72d
haidy <haidyabugom3a@gmail.com>
2024-03-12 05:47:40 +0200

    happy init :D

---
+++ section1/task1.py
@@ -0,0 +1,28 @@
+from nltk.tokenize import word_tokenize, sent_tokenize, MWETokenizer
+
+def tokenize_text(text, option):
+    if(option == 1):
+        return word_tokenize(text)
+    if(option == 2):
+        return sent_tokenize(text)
+    return text.split()
+        
+def show_prombt_message():
+    print('select option for tokenization:')
+    print('select 1 for using word tokenize function')
+    print('select 2 for using sentence tokenize function')
+    print('select 3 for using split function')
+
+
+show_prombt_message()
+option = int(input())
+while(option not in [1,2,3]):
+    show_prombt_message()
+    option = int(input())
+
+text = "Hello, today i went home early. I feel very happy. I want to go to New York"
+tokenized_text = tokenize_text(text, option)
+mwe_list = [('New', 'York')]
+mwe_tokenizer = MWETokenizer(mwe_list)
+tokenized_text = mwe_tokenizer.tokenize(tokenized_text)
+print(tokenized_text)
